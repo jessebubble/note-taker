b14a1f2edfa76400e2fb447123fabe78bf1938d3
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-01-23 21:12:00 -0600

    readme update plus server.js updates to setup server

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# note-taker
-challenge 11 express js
+# Note-Taker
+Challenge 11 - Express.JS
+
+## Description
+Create an application called Note Taker that can be used to write and save notes. 
+This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+The application’s front end has already been created. It's your job to build the back end, connect the two, 
+and then deploy the entire application to Heroku.
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express'); // calls express
+const fs = require('fs'); // calls file system
+const path = require('path'); // node.js API - makes FS more predictable when working with Heroku
+
+const { notes } = require('./db/db.json') // access to json
+
+const app = express(); // Sets up express server - "instantiate the server"
+
+const PORT = process.env.PORT || 3001; // environment variable for Heroku to run app
+
+// middleware functions built into express.js
+// takes incoming POST data and converts it to key/value pairings 
+// extended true option informs server sub-array data may exist
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); // takes incoming POST data in the form of json and parses it
+app.use(express.static('public')); // gives access to front end code in public folder
+
+
+
+
+app.listen(PORT, () => { // // listens for any server request
+    console.log(`API server now on port ${PORT}`); 
+})
+
+// WHEN I open the Note Taker
+// THEN I am presented with a landing page with a link to a notes page
+// WHEN I click on the link to the notes page
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+// WHEN I enter a new note title and the note’s text
+// THEN a Save icon appears in the navigation at the top of the page
+// WHEN I click on the Save icon
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+// WHEN I click on an existing note in the list in the left-hand column
+// THEN that note appears in the right-hand column
+// WHEN I click on the Write icon in the navigation at the top of the page
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
